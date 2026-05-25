6a1c674d8425e9dea0ec3cbc31151db86b14f744
Issac Zheng <issac.zheng9869@gmail.com>
2026-05-25 08:00:37 -0400

    feat: add THR_EXT_MARKER constant to messaging

--- src/messaging.ts
+++ src/messaging.ts
@@ -7,6 +7,8 @@ export const THR_CONTEXT_STRIP_RE = new RegExp(
   `^<${THR_CONTEXT_TAG}>\\n[\\s\\S]*?\\n<\\/${THR_CONTEXT_TAG}>\\n\\n`,
 )
 
+export const THR_EXT_MARKER = '<threads-ext-marker/>'
+
 export interface StageSummaryMsg {
   type: typeof MSG.STAGE_SUMMARY
   summaryTexts: string[]
